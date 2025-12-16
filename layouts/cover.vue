0f25538a092582ed9b69144d9d757845367055ad
Kjetil Mehl <kjetil.mehl@husbanken.no>
2025-12-16 08:45:30 +0100

    Fikser gap

--- layouts/cover.vue
+++ layouts/cover.vue
@@ -2,10 +2,11 @@
   <div class="slidev-layout cover relative h-full">
     <div class="my-auto w-full">
       <slot />
-      <div class="-ml-0.75rem flex justify-start items-center gap-8 mt-16">
+      <div class="-ml-0.75rem flex justify-start items-center gap-2 mt-16">
         <div class="text-center">
           <div>{{ $slidev.configs.date }}</div>
         </div>
+        <div>|</div>
         <div class="text-center">
           <div>{{ $slidev.configs.author }}</div>
         </div>
